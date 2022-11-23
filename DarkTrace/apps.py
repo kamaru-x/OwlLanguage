1712c37e538f3ba6fc7de6f5208ad2b2de9c5e42
Kamaru <xin.kamaru@gmail.com>
2022-11-23 12:10:16 +0530

    remove frontend

--- DarkTrace/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DarktraceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'DarkTrace'
--- GeneralWebsite/settings.py
+++ GeneralWebsite/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'products',
     'services',
     'testimonials',
-    'DarkTrace',
 ]
 
 MIDDLEWARE = [
